8f363ef03ba02d53886b278e7e5125c6cf2d9af9
Manish Trivedi <manish.tech2@gmail.com>
2022-10-19 18:10:49 +0530

    task: integrated xtravision AAR file

--- app/src/main/java/com/xtra/kotlin/example/MainActivity.kt
+++ app/src/main/java/com/xtra/kotlin/example/MainActivity.kt
@@ -3,35 +3,73 @@ package com.xtra.kotlin.example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//import ai.xtravision.*
-////import ai.xtravision.base.*
+import ai.xtravision.*
+import ai.xtravision.util.*
+import androidx.camera.view.PreviewView
 
+import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), XtraVisionAIListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // STEP-1:: Set Required Variable
+        val authToken = "_AUTH-TOKEN_"
+        val assessmentName = "BANDED_EXTERNAL_ROTATION"
+        val isPreJoin = false
+        val previewView : PreviewView = findViewById<PreviewView>(R.id.xtraVisionPreviewView)
+        val selectedCamera = 1  //0: Front Camera, 1: Back Camera
 
+        // STEP-2:: create initial Objects: connectionData,requestData and libData
+        val connectionData = XtraVisionConnectionData(authToken, assessmentName)
+        val reqData = XtraVisionRequestData(isPreJoin)
+        val libData = XtraVisionLibData(
+            this,
+            previewView,
+            this,
+            selectedCamera
+        )
 
-//        val connectionData = ConnectionData(authToken, assessmentName)
-//        val reqData = RequestData(isPreJoin)
-//        val libData = LibData(
-//            this,
-//            previewView,
-//            this,
-//            selectedCamera
-//        )
-//
-//        // Check and Ask Camera permission
-//        PermissionHelper.checkAndRequestCameraPermissions(this)
-//
-//        // if permission is granted, then let's start assessment
-//        if (PermissionHelper.cameraPermissionsGranted(this)) {
-//            val xtraVisionAIManger = XtraVisionAssessment(connectionData, reqData, libData)
-//            xtraVisionAIManger!!.initiate(this)
-//        } else {
-//            PermissionHelper.checkAndRequestCameraPermissions(this)
-//        }
+        // Check and Ask for Camera permission
+        XtraVisionPermissionHelper.checkAndRequestCameraPermissions(this)
+
+        // STEP-3:: if permission is granted, then let's start an assessment
+        if (XtraVisionPermissionHelper.cameraPermissionsGranted(this)) {
+            // create an object and initiate the Assessment process
+            val xtraVisionAIManger = XtraVisionAIManager(
+                connectionData, reqData, libData)
+            xtraVisionAIManger!!.initiate()
+        } else {
+            XtraVisionPermissionHelper.checkAndRequestCameraPermissions(this)
+        }
+
     }
+
+
+    /**
+     * ==============================
+     * Response Listener: Start
+     * ==============================
+     */
+    override fun onConnectSuccess() {
+        Log.d("CustomAIListener","onConnectSuccess")
+    }
+
+    override fun onConnectFailed() {
+        Log.e("CustomAIListener","onConnectFailed")
+    }
+
+    override fun onConnectClose() {
+        Log.d("CustomAIListener","onConnectClose")
+    }
+
+    override fun onResponse(resp: String?) {
+        Log.d("CustomAIListener", resp.toString())
+    }
+    /**
+     * ==============================
+     * Response Listener: End
+     * ==============================
+     */
 }
